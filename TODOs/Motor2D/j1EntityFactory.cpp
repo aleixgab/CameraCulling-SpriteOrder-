4a4a7f0c5c77b9adc9cf4348a605962347ae960c
aleixgab <aleix.gab@gmail.com>
2018-03-04 00:26:50 +0100

    TODOs and solutions

---
+++ TODOs/Motor2D/j1EntityFactory.cpp
@@ -0,0 +1,82 @@
+#include "j1EntityFactory.h"
+#include "j1Render.h"
+#include "p2Log.h"
+#include "Player.h"
+
+bool j1EntityFactory::Start() {
+
+	texture = App->tex->Load("textures/entities2.png");
+	return true;
+}
+
+bool j1EntityFactory::PreUpdate() {
+
+	//TODO 6: Iterate entities list and push entities information in the priority queue
+	//Remember the firsts TODOs from Camera Culling. Dont push in the queue if entity is not in the screen
+
+	return true;
+}
+
+
+bool j1EntityFactory::Update(float dt)
+{
+	//TODO 7: Pop queue and blit one by one
+	//coment [(*iterator)->Draw(texture);] it's a few lines above
+
+
+	for (std::list<Entity*>::iterator iterator = entities.begin(); iterator != entities.end(); iterator++) {
+		(*iterator)->Draw(texture);
+		(*iterator)->Move(dt);
+	}
+
+
+	
+	return true;
+}
+
+bool j1EntityFactory::CleanUp()
+{
+	App->tex->UnLoad(texture);
+	for (std::list<Entity*>::iterator iterator = entities.begin(); iterator == entities.end(); iterator++)
+	{
+		delete *iterator;
+	}
+	entities.clear();
+	return true;
+}
+
+Entity* j1EntityFactory::AddEntity(int pos_x, int pos_y, ENTITY_TYPES type)
+{
+	Entity* ent = nullptr;
+	if(type == PLAYER)
+		 ent = new Player(pos_x, pos_y, type);
+	else
+		ent = new Entity(pos_x,pos_y, type);
+
+	ent->type = type;
+	ent->pos = { pos_x, pos_y };
+	
+	if (ent->type == GRUNT) 
+		ent->rect = { 269, 0, 45, 45 };
+
+	else if (ent->type == AXE_THROWER) 
+		ent->rect = { 227, 0, 45, 45 };
+
+	else if (ent->type == TOWN_HALL) 
+		ent->rect = { 225, 60, 120, 115 };
+	
+	else if (ent->type == PLAYER) 
+		ent->rect = { 0, 0, 50, 55 };
+	
+
+	entities.push_back(ent);
+
+	return ent;
+}
+
+
+void Entity::Draw(SDL_Texture* texture)
+{
+	App->render->Blit(texture, pos.x, pos.y, &rect);
+}
+
---
+++ TODOs/Motor2D/j1EntityFactory.h
@@ -0,0 +1,73 @@
+#ifndef __J1ENTITYFACTORY_H__
+#define __J1ENTITYFACTORY_H__
+
+#include "j1App.h"
+#include "j1Module.h"
+#include "j1Render.h"
+#include "j1Textures.h"
+#include "SDL/include/SDL.h"
+#include "Animation.h"
+
+#include <list>
+#include <queue>
+
+#define MAX_ENEMIES 5
+
+class Entity;
+
+enum ENTITY_TYPES
+{
+	NO_TYPE,
+	PLAYER,
+	GRUNT,
+	AXE_THROWER,
+	TOWN_HALL
+};
+
+//TODO 3: Create a struct from entities information that holds their rect, position, animation and Priority(it's a positive whole)
+
+
+//TODO 5: Create a struct that will compare entities information priorities
+
+class j1EntityFactory : public j1Module {
+private:
+
+	std::list<Entity*> entities;
+
+//	TODO 4: Create a priority_queue with struct made it on last TODO
+
+
+public:
+	j1EntityFactory() {};
+	virtual ~j1EntityFactory() {};
+
+public:
+	bool PreUpdate();
+	bool Update(float dt);
+	bool CleanUp();
+	bool Start();
+
+	Entity* AddEntity(int x, int y, ENTITY_TYPES type);
+
+	SDL_Texture* texture;
+
+};
+
+class Entity {
+public:
+
+	Entity(int x, int y, ENTITY_TYPES type) {}
+	virtual void Draw(SDL_Texture* texture);
+	virtual void Move(float dt) {};
+
+public:
+	iPoint pos;
+	ENTITY_TYPES type;
+	SDL_Rect rect;
+	uint priority;
+	Animation* CurrentAnim = nullptr;
+private:
+
+};
+
+#endif // !__ENTITIES_H__
